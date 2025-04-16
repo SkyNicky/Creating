88d4ba055d520ee0a82142d3db3905c85d1f8e8a
Nycholas Dev <143964335+SkyNicky@users.noreply.github.com>
2025-04-16 20:00:09 -0300

    Update Sobre.jsx

--- src/Componentes/Sobre.jsx
+++ src/Componentes/Sobre.jsx
@@ -1,6 +1,7 @@
 import styles from './Sobre.module.css';
 import backgroundImage from '../assets/Img/jesson-mata-I2Erl2cU_A4-unsplash.jpg';
 import companyImage from '../assets/Img/mathias-reding-H-8NJ0T_9Hk-unsplash.jpg';
+
 export function Sobre(props) {
     return (
         <div className={styles.container}>
@@ -14,8 +15,8 @@ export function Sobre(props) {
                     <img src={companyImage} alt="Loja" className={styles.storeImage} />
                 </div>
                 <div className={styles.textContainer}>
-                <h2 className={styles.subtitle}>Sobre a Loja</h2>
-                    <p>
+                    <h2 className={styles.subtitle}>Sobre a Loja</h2>
+                    <p className={styles.paragraph}>
                         No Mix Burguers, buscamos proporcionar uma experiência gastronômica única, oferecendo uma variedade de hambúrgueres, hot dogs e porções deliciosas. Nossa lanchonete tem como objetivo ser um ponto de encontro acolhedor para amigos e famílias desfrutarem de refeições saborosas em um ambiente descontraído.
                     </p>
                     <h2 className={styles.subtitle}>Missão</h2>
