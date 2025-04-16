3a6198307331d9353594733d281d640af256baa3
Nycholas Dev <143964335+SkyNicky@users.noreply.github.com>
2025-04-16 20:07:13 -0300

    Update Footer.jsx

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,19 +1,19 @@
-import { Link } from 'react-router-dom';
-import styles from './Footer.module.css';
+import { Link } from 'react-router-dom'
+import styles from './Footer.module.css'
 
 export function Footer() {
   return (
+
     <footer className={styles.footer}>
-      <div className={styles.footer1}>
-        <div className={styles.infoFooter1}>
-          <p>Horário de Funcionamento: Ter/Dom - 18:00 às 00:00</p>
-          <p>Rua Humberto Mauro, nº 777, Centro, Cataguases-MG</p>
-          <Link to="/contato">Fale conosco</Link>
+        <div className={styles.footer1}>
+            <div className={styles.infoFooter1}>
+            <p href="#">Horário de Funcionamento: Ter/Dom - 18:00 às 00:00!</p>
+            <p href="#">Rua Humberto Mauro, nº 777, Centro, Cataguases-MG</p>
+            </div>
         </div>
-      </div>
-      <section>
-        <p className={styles.copyright}>© Mix Burguers</p>
-      </section>
+        <section>
+            <p className={styles.copyright}>© Mix Burguers</p>
+        </section>
     </footer>
-  );
+  )
 }
