8412d6e50069caa4a76421a7f74221f93d3b76d8
Nycholas Dev <143964335+SkyNicky@users.noreply.github.com>
2025-04-16 19:38:47 -0300

    Update dataMenu.js

--- src/Componentes/dataMenu.js
+++ src/Componentes/dataMenu.js
@@ -1,21 +1,25 @@
 export const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 1024 },
-      items: 4,
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 800 },
-      items: 4,
-    },
-    tablet: {
-      breakpoint: { max: 800, min: 464 },
-      items: 4,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 4,
-    }
-  };
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1600 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 1600, min: 1024 },
+    items: 4,
+  },
+  laptop: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
   // Importações de imagens// Importações de imagens
 import ham1 from '../assets/Img/ham1.jfif';
 import ham2 from '../assets/Img/hambuguer_com_3_bife_cheddar_e_bacon.jfif';
